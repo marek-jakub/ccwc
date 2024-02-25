95b306809f49b51e082fad91abb5878319c81e63
marek-jakub <marekjakub@hotmail.co.uk>
2024-02-25 15:34:56 +0100

    Update tests.

--- test_ccwc.py
+++ test_ccwc.py
@@ -5,7 +5,7 @@ import ccwc
 
 class TestCcwc(unittest.TestCase):
 
-    def test_file_size(self):
+    def test_input_measurements(self):
         self.assertEqual(
             ccwc._measure_content(ccwc._read_file('test.txt')),
             (7145, 58164, 342190, 339292), 'Incorrect measurements.')
